9cfd25f7c0bf792dae49a8b590f09461766e1486
JuneChul Roh <jroh@ti.com>
2022-11-11 18:06:30 -0600

    [scirpts] Fixed scripts/install_imx390_ldc_files.sh

--- scripts/install_imx390_ldc_files.sh
+++ scripts/install_imx390_ldc_files.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
+# project path
+ARCH=`arch`
+PROJ_NAME=ti-d3-mobile-robot-demos
+if [[ "$ARCH" == "aarch64" ]]; then
+    PROJ_DIR=/opt/$PROJ_NAME
+elif [[ "$ARCH" == "x86_64" ]]; then
+    PROJ_DIR=$HOME/j7ros_home/tid3_ws/src/$PROJ_NAME
+else
+    echo "$ARCH is not supported"
+    exit 1
+fi
+
 unzip $PROJ_DIR/scripts/tda4_files/imx390_35244_ldc_bin_files.zip \
     -d /opt/imaging/imx390
